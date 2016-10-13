e1a623228debaba6b769da5dd5b5c5db3003d1cf
Oleg <gafrom@gmail.com>
2016-10-13 11:03:04 +0300

    fix typo

--- lib/release_notifier.rb
+++ lib/release_notifier.rb
@@ -2,7 +2,7 @@ require 'release_notifier/version'
 require 'release_notifier/changelog'
 
 module ReleaseNotifier
-  class Appication
+  class Application
     def self.call(env)
       ['200', {}, ['Hello from release_notifier']]
     end
